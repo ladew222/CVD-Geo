d03091e34c282fcfc21093d4b22f5ddd137e2068
ericweinberg <egweinberg@viterbo.edu>
2020-04-02 13:09:06 -0500

    style updates

--- pearson-correlation.js
+++ pearson-correlation.js
@@ -1,7 +1,22 @@
-function pearson(x, y){
-  let promedio = (lista) => { return lista.reduce((s, a) => s + a, 0) / lista.length };
-  let n = x.length, prom_x = promedio(x) , prom_y = promedio(y);
-  return (x.map( (e, i, r) => (r = {x:e, y:y[i]}) ).reduce( (s, a) => s + a.x * a.y, 0) - n * prom_x * prom_y) /
-      ((Math.sqrt(x.reduce( (s, a) => (s + a * a) , 0) - n * prom_x * prom_x)) *
-          (Math.sqrt(y.reduce( (s, a) => (s + a * a) , 0) - n * prom_y * prom_y)));
-} 
+const pearson = (x, y) => {
+  let sumX = 0,
+      sumY = 0,
+      sumXY = 0,
+      sumX2 = 0,
+      sumY2 = 0;
+  const minLength = x.length = y.length = Math.min(x.length, y.length),
+      reduce = (xi, idx) => {
+        const yi = y[idx];
+        sumX += xi;
+        sumY += yi;
+        sumXY += xi * yi;
+        sumX2 += xi * xi;
+        sumY2 += yi * yi;
+      }
+  x.forEach(reduce);
+  return (minLength * sumXY - sumX * sumY) / Math.sqrt((minLength * sumX2 - sumX * sumX) * (minLength * sumY2 - sumY * sumY));
+};
+let arrX = [20, 54, 54, 65, 45];
+let arrY = [22, 11, 21, 34, 87];
+let R = pcorr(arrX, arrY);
+console.log('arrX', arrX, 'arrY', arrY, 'R', R);
